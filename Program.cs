bd4a90c7c623a50bc7e931d36a958fc87f6f8d4c
Daniela Castro <dcastrog1@eafit.edu.co>
2025-02-16 10:43:06 -0500

    Add ApplicationDbContext and configure database connection

--- Program.cs
+++ Program.cs
@@ -1,4 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using AztroWebApplication.Data;
+using AztroWebApplication.Repositories;
+using AztroWebApplication.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Guardar la cadena de conexión en una variable
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+//Validar que la cadena de conexión no sea nula o vacía
+if(string.IsNullOrEmpty(connectionString))
+{
+    throw new InvalidOperationException("Connection string is empty or null");
+}
+
+builder.Services.AddDbContext<AztroWebApplication.Data.ApplicationDbContext>(options => options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<UserService>();
+
 
 // Add services to the container.
 
